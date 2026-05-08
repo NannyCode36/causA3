0ddfb51a2868c8ccfb93332ed730b0abe77a7227
Edilane <edilanedasilvaalves@gmail.com>
2026-05-07 21:19:50 -0300

    :package: Ajuste do Package  json e main

---
+++ src/main/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Carregar a página inicial
+  mainWindow.loadFile('src/renderer/pages/inicio/home_page.html');
+
+  // Abrir DevTools em modo desenvolvimento
+  if (process.argv.includes('--dev')) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
